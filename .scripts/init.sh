98a208eff61240b5953d862beebd1968f081c669
nametake <nametake.kyarabuki@gmail.com>
2015-04-21 12:45:28 +0900

    add pyenv-virtualenv install script to init.sh

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
 git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
