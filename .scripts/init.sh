76f954eeb0fd24a9761d2d455c881b455353fcec
nametake <nametake.kyarabuki@gmail.com>
2015-10-19 12:26:59 +0900

    add if stmt to init.sh

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 
 # zsh antigen
-git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+if ! [ -e ${HOME}/.antigen ]; then
+  echo "angigen is already installed."
+else
+  git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+  antigen bundle
+fi
 
 # pyenv
-git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
-git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
-git clone git://github.com/yyuu/pyenv-update.git ~/.pyenv/plugins/pyenv-update
-git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-pip-rehash
+if [ -e ${HOME}/.pyenv ]; then
+  echo "pyenv is already installed."
+else
+  git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
+  git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
+  git clone git://github.com/yyuu/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+  git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-pip-rehash
+fi
 
-# grunt
-npm install -g grunt-init
-git clone https://github.com/gruntjs/grunt-init-jquery.git ~/.grunt-init/jquery
-git clone https://github.com/nosami/grunt-init-csharpsolution.git ~/.grunt-init/csharpsolution
+if [ -x `which npm` ]; then
+  # grunt
+  npm install -g grunt-init
+  git clone https://github.com/gruntjs/grunt-init-jquery.git ~/.grunt-init/jquery
+  git clone https://github.com/nosami/grunt-init-csharpsolution.git ~/.grunt-init/csharpsolution
+else
+  echo "npm not found."
+fi
 
 # Golang
-go get github.com/nsf/gocode
+if [ -x `which go` ]; then
+  go get github.com/nsf/gocode
+  go get github.com/peco/peco/cmd/peco
+else
+  echo "go not found."
+fi
