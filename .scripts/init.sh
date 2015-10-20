6edb392d76fd942fa79e09e3addae2085fa463fb
nametake <nametake.kyarabuki@gmail.com>
2015-10-20 17:34:58 +0900

    change init.sh

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -1,2 +1,44 @@
 #!/bin/sh
 
+cd `dirname $0` || exit 1
+. lib.sh
+
+# angigen
+if ! [ -e ${HOME}/.antigen ]; then
+  echo "angigen is already installed."
+else
+  git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+  antigen bundle
+fi
+
+# pyenv
+if [ -e ${HOME}/.pyenv ]; then
+  echo "pyenv is already installed."
+else
+  git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
+  git clone https://github.com/yyuu/pyenv-virtualenv.git\
+    $HOME/.pyenv/plugins/pyenv-virtualenv
+  git clone git://github.com/yyuu/pyenv-update.git\
+    ~/.pyenv/plugins/pyenv-update
+  git clone https://github.com/yyuu/pyenv-pip-rehash.git\
+    ~/.pyenv/plugins/pyenv-pip-rehash
+fi
+
+# npm tools
+if [ `exists_command npm` -eq 0 ]; then
+  npm install -g grunt-init
+  git clone https://github.com/gruntjs/grunt-init-jquery.git\
+    ~/.grunt-init/jquery
+  git clone https://github.com/nosami/grunt-init-csharpsolution.git\
+    ~/.grunt-init/csharpsolution
+else
+  echo "npm not found."
+fi
+
+# golang
+if [ `exists_command go` -eq 0 ]; then
+  go get github.com/nsf/gocode
+  go get github.com/peco/peco/cmd/peco
+else
+  echo "go not found."
+fi
