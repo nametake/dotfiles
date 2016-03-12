b1f0a4b3214a79029392d867da96c2d3c843003f
nametake <nametake.kyarabuki@gmail.com>
2016-03-12 16:56:39 +0900

    Fix create-links.sh

--- .scripts/create-links.sh
+++ .scripts/create-links.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-ln -s ../.zshenv $HOME/.zshenv
-ln -s ../.zshrc $HOME/.zshrc
-ln -s ../.zshrc.zplug $HOME/.zshrc.zplug
-ln -s ../.vimrc $HOME/.vimrc
-ln -s ../.vim $HOME/.vim
+
+if [ -e ${HOME}/dotfiles ]; then
+  ln -s ${HOME}/dotfiles/.zshenv ${HOME}/.zshenv
+  ln -s ${HOME}/dotfiles/.zshrc ${HOME}/.zshrc
+  ln -s ${HOME}/dotfiles/.zshrc.zplug ${HOME}/.zshrc.zplug
+  ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
+  ln -s ${HOME}/dotfiles/.vim ${HOME}/.vim
+else
