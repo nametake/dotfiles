2e7184f29b8ea35730e6799ab771147422af1495
nametake <nametake.kyarabuki@gmail.com>
2016-04-13 02:08:15 +0900

    Split pyenv

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -7,19 +7,6 @@ else
   curl -fLo ~/.zplug/zplug --create-dirs git.io/zplug
 fi
 
-# pyenv
-if [ -e ${HOME}/.pyenv ]; then
-  echo "pyenv is already installed."
-else
-  git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
-  git clone https://github.com/yyuu/pyenv-virtualenv.git\
-    $HOME/.pyenv/plugins/pyenv-virtualenv
-  git clone git://github.com/yyuu/pyenv-update.git\
-    ~/.pyenv/plugins/pyenv-update
-  git clone https://github.com/yyuu/pyenv-pip-rehash.git\
-    ~/.pyenv/plugins/pyenv-pip-rehash
-fi
-
 if [ -e ${HOME}/.tmux/plugins/tpm ]; then
   echo "tpm(tmux plugin manager) is already installed."
 else
