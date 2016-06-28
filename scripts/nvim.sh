b72ffd48fb9b4becf07070b79149574bfdb614f0
nametake <nametake.kyarabuki@gmail.com>
2016-06-28 15:31:52 +0900

    Fix nvim.sh

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
 vim="${HOME}/.vim"
-vimrc="${HOME}/.vimrc"
-
 
 if [ -f ${vim} -o -L ${vim} ]; then
   ln -s ${vim} ${XDG_CONFIG_HOME}/nvim
   echo "Create .vim link."
 else
   echo "Not .vim dir"
-fi
-
-if [ -f ${vimrc} -o -L ${vimrc} ]; then
-  ln -s ${vimrc} ${XDG_CONFIG_HOME}/nvim/init.vim
-  echo "Create .vimrc link."
-else
-  echo "Not .vimrc file"
 fi
 
 
