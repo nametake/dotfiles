0fdbbf954520072c55e084bebfc8ca2c11114a71
nametake <nametake.kyarabuki@gmail.com>
2017-06-06 11:20:43 +0900

    Add fish prompt config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,7 @@
+set -g __fish_git_prompt_char_stagedstate "@"
 set -g __fish_git_prompt_char_dirtystate "+"
+set -g __fish_git_prompt_char_untrackedfiles "_"
+set -g __fish_git_prompt_char_conflictedstate "*"
+set -g __fish_git_prompt_char_cleanstate "#"
+
 set -U GHQ_SELECTOR peco
