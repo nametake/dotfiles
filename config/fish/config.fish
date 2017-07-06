c4e93873d46e719fba8c3654c035c1ef5dbfcb96
nametake <nametake.kyarabuki@gmail.com>
2017-07-06 10:31:35 +0900

    Add gvim alias to fish.config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,7 @@ set -g __fish_git_prompt_char_conflictedstate "*"
 set -g __fish_git_prompt_char_cleanstate "#"
 
 set -U GHQ_SELECTOR peco
+
+if test -d /Applications/MacVim.app
+  alias gvim="/Applications/MacVim.app/Contents/MacOS/gvim"
+end
