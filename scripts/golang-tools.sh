0b653d5b42a17b5f64d2215a60316148ef4029bc
nametake <nametake.kyarabuki@gmail.com>
2018-06-23 17:37:58 +0900

    remove scripts

--- scripts/golang-tools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-go get github.com/peco/peco/cmd/peco
-
-# vim
-go get -u github.com/nsf/gocode
-go get -u github.com/rogpeppe/godef
-go get -u github.com/jstemmer/gotags
-go get -u github.com/golang/lint/golint
