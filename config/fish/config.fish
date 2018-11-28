85e5baa2ce919fe531ab006a5580e0d8eeab011b
nametake <nametake.kyarabuki@gmail.com>
2018-11-28 23:22:40 +0900

    Add fisher cmd

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,3 +11,9 @@ alias g='git'
 if test -e /Applications/MacVim.app
   alias gvim='/Applications/MacVim.app/Contents/bin/gvim'
 end
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
