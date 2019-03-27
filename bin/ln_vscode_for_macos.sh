f49e9ec7383be428bb53422dc674b803b97d39d0
nametake <nametake.kyarabuki@gmail.com>
2019-03-27 11:49:08 +0900

    Add config scripts

--- bin/ln_vscode_for_macos.sh
+++ bin/ln_vscode_for_macos.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if [ ! -e ${HOME}/.config/Code/User ]; then
+vscode_dotfiles_dir=${HOME}/.config/Code/User
+
+if [ ! -e ${vscode_dotfiles_dir} ]; then
   echo "VSCode config is not found."
   exit 1
 fi
-echo foo
+
+vscode_config_dir=${HOME}/Library/Application\ Support/Code/User
+config_files=(settings.json keybindings.json)
+
+for file in ${config_files[@]}; do
+  ln -ins "${vscode_dotfiles_dir}/${file}" "${vscode_config_dir}/${file}"
+done
