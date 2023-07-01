90f9c69e3cb943402609ede2fde7c09dd82293a2
nametake <nametake.kyarabuki@gmail.com>
2023-07-01 23:05:09 +0900

    Added lsp_init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -7,66 +7,25 @@ if (vim.fn.IsPlugged('vim-lsp') == 1) then
 end
 
 require("lsp_handler").setup()
-
--- LSP setting
-local servers = {
-  -- efm-langserver
-  efm = {},
-  lua_ls = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = {'vim'},
+require("lsp_init").setup({
+  servers = {
+    -- efm-langserver
+    efm = {},
+    lua_ls = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = {'vim'},
+          },
         },
       },
     },
-  },
-  rust_analyzer = {},
-  gopls = {},
-  tsserver = {},
-}
+    rust_analyzer = {},
+    gopls = {},
+    tsserver = {},
+  }
+})
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local lspconfig = require('lspconfig')
-
-local function merge(list1, list2)
-  local result = {}
-  for key, value in pairs(list1) do
-    result[key] = value
-  end
-  for key, value in pairs(list2) do
-    result[key] = value
-  end
-
-  return result
-end
-
-local function keys(list)
-  local keyset={}
-  local n=0
-
-  for k, _ in pairs(list) do
-    n=n+1
-    keyset[n]=k
-  end
-  return keyset
-end
-
-require("mason").setup()
-require("mason-lspconfig").setup {
-  ensure_installed = keys(servers),
-}
-
-local default_options = {
-  -- on_attach = my_custom_on_attach,
-  capabilities = capabilities,
-}
-
-for lsp, options in pairs(servers) do
-  lspconfig[lsp].setup(
-    merge(default_options, options)
-  )
-end
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
