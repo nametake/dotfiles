e3f0bd729452f4416a5fe863bac392d042b2db88
nametake <nametake.kyarabuki@gmail.com>
2024-01-16 19:23:14 +0900

    Fixed prompt char

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,22 @@
 set -g __fish_git_prompt_showuntrackedfiles true
 
-set -g __fish_git_prompt_char_stagedstate "@"
-set -g __fish_git_prompt_char_dirtystate "+"
-set -g __fish_git_prompt_char_untrackedfiles "_"
-set -g __fish_git_prompt_char_conflictedstate "*"
-set -g __fish_git_prompt_char_cleanstate "#"
+# set -g __fish_git_prompt_char_cleanstate "#"
+# set -g __fish_git_prompt_char_invalidstate '*'
+# set -g __fish_git_prompt_char_stagedstate "@"
+# set -g __fish_git_prompt_char_dirtystate "+"
+# set -g __fish_git_prompt_char_untrackedfiles "_"
+# set -g __fish_git_prompt_char_conflictedstate "*"
+
+set -g __fish_git_prompt_char_cleanstate ''
+set -g __fish_git_prompt_char_dirtystate '*'
+set -g __fish_git_prompt_char_invalidstate '#'
+set -g __fish_git_prompt_char_stagedstate '+'
+set -g __fish_git_prompt_char_stashstate '$'
+set -g __fish_git_prompt_char_untrackedfiles '%'
+set -g __fish_git_prompt_char_upstream_ahead '>'
+set -g __fish_git_prompt_char_upstream_behind '<'
+set -g __fish_git_prompt_char_upstream_diverged '<>'
+set -g __fish_git_prompt_char_upstream_equal '='
 
 set -U GHQ_SELECTOR peco
 
