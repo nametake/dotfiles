37cbabc60e3678bc24777c5cc558190dbf534b8e
nametake <nametake.kyarabuki@gmail.com>
2025-03-22 01:08:19 +0900

    fix: improve treesitter config formatting and diagnostics
    - Add diagnostic comments to suppress TypeScript errors
    - Convert object literal style to consistent parentheses format
    - Adjust indentation from 4 spaces to 2 spaces for better readability
    - Maintain same functionality while improving code style

--- vim/lua/plugins/nvim-treesitter.lua
+++ vim/lua/plugins/nvim-treesitter.lua
@@ -1,47 +1,49 @@
 local Plugin = {}
 
 Plugin.setup = function()
-    require 'nvim-treesitter.configs'.setup {
-        ensure_installed = "all",
-        ignore_install = { "phpdoc" },
-        highlight = {
-            enable = true, -- false will disable the whole extension
-            -- disable = { "html" },
-            additional_vim_regex_highlighting = false,
-        },
-        indent = {
-            enable = true,
-        },
-    }
+  ---@diagnostic disable-next-line: missing-fields
+  require 'nvim-treesitter.configs'.setup({
+    ensure_installed = "all",
+    ignore_install = { "phpdoc" },
+    highlight = {
+      enable = true, -- false will disable the whole extension
+      -- disable = { "html" },
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+  })
 
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-    parser_config.bruno = {
-        install_info = {
-            url = "https://github.com/Scalamando/tree-sitter-bruno",
-            files = { 'src/parser.c', 'src/scanner.c' },
-            branch = "main", -- default branch in case of git repo if different from master
-            -- generate_requires_npm = true,          -- if stand-alone parser without npm dependencies
-            -- requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
-        },
-        filetype = "bruno",
-    }
+  local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+  ---@diagnostic disable-next-line: inject-field
+  parser_config.bruno = {
+    install_info = {
+      url = "https://github.com/Scalamando/tree-sitter-bruno",
+      files = { 'src/parser.c', 'src/scanner.c' },
+      branch = "main", -- default branch in case of git repo if different from master
+      -- generate_requires_npm = true,          -- if stand-alone parser without npm dependencies
+      -- requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+    },
+    filetype = "bruno",
+  }
 
-    require 'treesitter-context'.setup {
-        enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20,     -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-    }
+  require 'treesitter-context'.setup({
+    enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+    max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+    min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+    line_numbers = true,
+    multiline_threshold = 20, -- Maximum number of lines to show for a single context
+    trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+    -- Separator between context and content. Should be a single character string, like '-'.
+    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    separator = nil,
+    zindex = 20,     -- The Z-index of the context window
+    on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+  })
 
-    require('nvim-ts-autotag').setup()
+  require('nvim-ts-autotag').setup()
 end
 
 return Plugin
