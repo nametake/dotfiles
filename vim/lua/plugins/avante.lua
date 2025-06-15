091e35c834005da34e8c13296a580604baedb522
nametake <nametake.kyarabuki@gmail.com>
2025-06-15 14:38:58 +0900

    Added clear command

--- vim/lua/plugins/avante.lua
+++ vim/lua/plugins/avante.lua
@@ -1,20 +1,20 @@
 local Plugin = {}
 
-local api =                 require('avante.api')
-
+local api = require('avante.api')
 
 Plugin.setup = function()
-  -- require('copilot').setup()
   require('avante').setup({
     provider = 'copilot',
     model = 'claude-sonnet-4',
     mappings = {
       ask = '<C-u>',
       edit = "<Space><C-u>",
-    }
+    },
   })
   vim.keymap.set('n', '<C-u>', api.ask, { noremap = true, silent = true })
   vim.keymap.set('v', '<C-u>', api.ask, { noremap = true, silent = true })
+
+  vim.keymap.set('n', '<C-l>', ':AvanteClear<CR>', { noremap = true, silent = true })
 end
 
 return Plugin
