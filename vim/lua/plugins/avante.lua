3bfba2ae77be63eb3a0dbc3be90dbd1e2bde6b4e
nametake <nametake.kyarabuki@gmail.com>
2025-06-15 19:37:02 +0900

    Fixed

--- vim/lua/plugins/avante.lua
+++ vim/lua/plugins/avante.lua
@@ -3,12 +3,19 @@ local Plugin = {}
 local api = require('avante.api')
 
 Plugin.setup = function()
+  ---@diagnostic disable-next-line:redundant-parameter
   require('avante').setup({
     provider = 'copilot',
     model = 'claude-sonnet-4',
     mappings = {
       ask = '<C-u>',
       edit = "<Space><C-u>",
+      sidebar = {
+        close_from_input = {
+          normal = "<C-c>",
+          insert = "<C-c>",
+        },
+      },
     },
   })
   vim.keymap.set('n', '<C-u>', api.ask, { noremap = true, silent = true })
