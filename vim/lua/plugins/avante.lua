ebcc2d4f9fdab1131beaba3d89cc28b563cf2843
nametake <nametake.kyarabuki@gmail.com>
2025-06-15 14:12:47 +0900

    Fixed avante map

--- vim/lua/plugins/avante.lua
+++ vim/lua/plugins/avante.lua
@@ -1,11 +1,19 @@
 local Plugin = {}
 
+local api =                 require('avante.api')
+
+
 Plugin.setup = function()
   -- require('copilot').setup()
   require('avante').setup({
     provider = 'copilot',
-    model = 'claude-sonnet-4'
+    model = 'claude-sonnet-4',
+    mappings = {
+      ask = '<space>u',
+    }
   })
+  vim.keymap.set('n', '<C-u>', api.ask, { noremap = true, silent = true })
+  vim.keymap.set('v', '<C-u>', api.ask, { noremap = true, silent = true })
 end
 
 return Plugin
