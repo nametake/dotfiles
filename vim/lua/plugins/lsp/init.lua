2fb4792b9f5ff5c0caaab049198070c896c18568
nametake <nametake.kyarabuki@gmail.com>
2025-11-12 11:32:03 +0900

    fix attatch

--- vim/lua/plugins/lsp/init.lua
+++ vim/lua/plugins/lsp/init.lua
@@ -44,6 +44,13 @@ Plugin.setup = function(opts)
   })
 
   require("lsp-format").setup()
+  vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      require("lsp-format").on_attach(client, args.buf)
+    end,
+  })
+
   require "lsp_signature".setup({
     doc_lines = 0,
     max_width = 96,
