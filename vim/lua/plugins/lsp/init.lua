83571ecfb7d82247c17d79fa4f63358a975599c2
nametake <nametake.kyarabuki@gmail.com>
2026-03-28 02:21:39 +0900

    Fixed format

--- vim/lua/plugins/lsp/init.lua
+++ vim/lua/plugins/lsp/init.lua
@@ -1,5 +1,7 @@
 local Plugin = {}
 
+local format_on_save_group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = true })
+
 if not pcall(require, 'lspconfig') then
   Plugin.setup = function() end
   return Plugin
@@ -54,9 +56,7 @@ Plugin.setup = function(opts)
   })
 
   local default_options = {
-    -- on_attach = my_custom_on_attach,
     capabilities = capabilities,
-    on_attach = require("lsp-format").on_attach,
   }
 
   for lsp, options in pairs(opts.servers) do
@@ -66,28 +66,11 @@ Plugin.setup = function(opts)
     vim.lsp.config(lsp, merge(default_options, options))
   end
 
-  -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
   vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-      local params = vim.lsp.util.make_range_params()
-      params.context = { only = { "source.organizeImports" } }
-      -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-      -- machine and codebase, you may want longer. Add an additional
-      -- argument after params if you find that you have to write the file
-      -- twice for changes to be saved.
-      -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-      for cid, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-          if r.edit then
-            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-            vim.lsp.util.apply_workspace_edit(r.edit, enc)
-          end
-        end
-      end
-      vim.lsp.buf.format({ async = false })
-    end
+    group = format_on_save_group,
+    callback = function(args)
+      vim.lsp.buf.format({ bufnr = args.buf, async = false, })
+    end,
   })
 
   require("copilot").setup({
